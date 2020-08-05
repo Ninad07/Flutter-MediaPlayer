96ba388ad57ab83cf0a44739b6a2ad29542b80d6
Ninad07 <55745785+Ninad07@users.noreply.github.com>
2020-08-05 15:03:36 +0530

    main

---
+++ main.dart
@@ -0,0 +1,15 @@
+import 'package:MediaPlayer/content/media.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+main() {
+  runApp(BaseApp());
+}
+
+class BaseApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MediaApp();
+  }
+}
